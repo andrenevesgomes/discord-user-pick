04f2622c7bed08d646a3a38926a3f2ea1f8420d3
André Gomes <48434290+andrenevesgomes@users.noreply.github.com>
2023-02-06 22:46:35 +0000

    Create bot_config.py

---
+++ bot_config.py
@@ -0,0 +1,8 @@
+# Used in some messages
+BOT_NAME = 'Exploding Colaborador'
+
+# Command prefix
+BOT_PREFIX = '-'
+
+# Environment variable with your application token (see installation section of README.md)
+BOT_ENV_TOKEN = 'you token here'
