9b49ceb47b18179681c20f884c6d7f7b85419ec3
André Gomes <agomes@altyra.com>
2023-03-26 18:45:11 +0100

    :white_check_mark:  done bot :clap:

--- bot.py
+++ bot.py
@@ -1,64 +1,44 @@
 import os
-import gettext
-import random
+from dotenv import load_dotenv
 import discord
-import interactions
 from discord.ext import commands
-# from discord_slash import SlashCommand
-# from discord_slash.utils import manage_commands
+import random
 
-from bot_config import BOT_NAME, BOT_ENV_TOKEN
+load_dotenv('bot_config.env')  # Load environment variables from .env file
+token = os.getenv("DISCORD_TOKEN")
+bot_name = os.getenv("BOT_NAME")
 
-bot = interactions.Client(token=BOT_ENV_TOKEN)
+intents = discord.Intents.default()
+intents.members = True  # You need to enable the Members Intent to get information about members in voice channels.
 
-# Implementation
+bot = commands.Bot(command_prefix="-", intents=intents)
 
-def pick_impl(ctx, count):
-    if count < 1:
-        # [LOC] Used with ;pick <n> when n < 1.
-        return str('You must pick at least one member.')
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
 
+@bot.command(name="pick",
+             description="Pick a random user from a voice channel")
+async def pick_random_user(ctx):
+  # Get the voice channel that the command user is currently in
+  voice_state = ctx.author.voice
+  if not voice_state:
+    await ctx.send(
+      f"Tens que estar num canal de voz :loudspeaker: e o {bot_name} tem que ter acesso a este canal"
+    )
+    return
+  channel = voice_state.channel
 
-    author = ctx.author
-    if not (hasattr(author, 'voice') and author.voice and author.voice.channel):
-        # [LOC] Used with ;pick when user not in voice channel.
-        print(hasattr(author, 'voice'))
-        return str(
-            'You must be in a voice channel :loudspeaker: '
-            'and {} must have access to this channel.'
-        ).format(BOT_NAME)
+  # Get a list of users currently in the voice channel
+  members = channel.members
 
-    members = author.voice.channel.members
-    if len(members) < 2:
-        # [LOC] Used with ;pick when user is alone in voice channel.
-        return str(
-            'You are alone in this channel :thinking: Call some friends!\n'
-            'Do you have friends, are you?'
-        )
+  # Pick a random user from the list of members
+  random_member = random.choice(members)
 
-    if len(members) < count:
-        # [LOC] Used with ;pick <n> when n greater than number of members in voice channel.
-        return str('Too few members in this channel')
-
-    sample = random.sample(members, count)
-    return ', '.join([user.mention for user in sample])
+  # Send a message with the name of the picked user
+  await ctx.send(
+    f"O colaborador do canal {channel.name} que será sacrificado é o {random_member.mention}! Parabéns! :skull:"
+  )
 
 
-@bot.command(
-    name="pick_user",
-    description="Mamaki oh nabo!",
-    options=[
-        interactions.Option(
-            name="count",
-            description="members count (1 by default)",
-            type=interactions.OptionType.INTEGER,
-            required=False,
-        ),
-    ],
-)
-async def pick_user(ctx: interactions.CommandContext, count: 1):
-    # await ctx.send(f"You said '{text}' https://cdn.shopify.com/s/files/1/0345/9180/1483/files/ek-2-player-ek--optimized.png?v=1642104393!")
-    await ctx.send(content=(pick_impl(ctx, count)))
-
-# Run bot
-bot.start()
+bot.run(token)
--- bot_config.py
+++
@@ -1,8 +0,0 @@
-# Used in some messages
-BOT_NAME = 'Exploding Colaborador'
-
-# Command prefix
-BOT_PREFIX = '-'
-
-# Environment variable with your application token (see installation section of README.md)
-BOT_ENV_TOKEN = 'you token here'
